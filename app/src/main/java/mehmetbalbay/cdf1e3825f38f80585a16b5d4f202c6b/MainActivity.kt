9b575254dc6ba78a815a4c5af08dfeb8c19b948c
Mehmet Agah <mehmeta.balbayy@gmail.com>
2021-04-01 21:47:21 +0300

    Initialize Commit

---
+++ app/src/main/java/mehmetbalbay/cdf1e3825f38f80585a16b5d4f202c6b/MainActivity.kt
@@ -0,0 +1,11 @@
+package mehmetbalbay.cdf1e3825f38f80585a16b5d4f202c6b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
