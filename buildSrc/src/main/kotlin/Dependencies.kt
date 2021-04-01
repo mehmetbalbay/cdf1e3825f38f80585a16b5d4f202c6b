df37ca872d83d3f2be2522502e70a11c5d117234
Mehmet Agah <mehmeta.balbayy@gmail.com>
2021-04-01 22:25:05 +0300

    Libraries and versions added

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,69 @@
+object Dependencies {
+
+    // Kotlin Libs
+    val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinVersion}"
+    val coreCoreKtx = "androidx.core:core-ktx:${Versions.coreCoreKtx}"
+
+    // Android Libs
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+    val cardView = "androidx.cardview:cardview:${Versions.cardView}"
+
+    // Koin
+    val koinAndroid = "org.koin:koin-android:${Versions.koin}"
+    val koinAndroidXScope = "org.koin:koin-androidx-scope:${Versions.koin}"
+    val koinAndroidXViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    val koinAndroidXFragment = "org.koin:koin-androidx-fragment:${Versions.koin}"
+    val koinTest = "org.koin:koin-test:${Versions.koin}"
+
+    // Glide
+    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+
+    // Room
+    val roomRuntime = "androidx.room:room-runtime:${Versions.room}"
+    val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+
+    // Rest Libs
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofitGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val gson = "com.google.code.gson:gson:${Versions.gson}"
+    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+
+    // Coroutines
+    val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    val coroutineAndroidTest = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
+
+    // ViewModel LiveData
+    val archLifecycleViewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    val archLifecycleLivedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
+    val archLifecycle = "android.arch.lifecycle:extensions:${Versions.lifecycle}"
+
+    // Logging
+    val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    // Shared Pref
+    val hawk = "com.orhanobut:hawk:${Versions.hawk}"
+
+    // RX JAVA ANDROID
+    val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
+    val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+
+    // MultiDex
+    val multidex = "androidx.multidex:multidex:${Versions.multidex}"
+
+    // Material Design Lib
+    var androidMaterial = "com.google.android.material:material:${Versions.androidMaterial}"
+
+    // Navigation Component
+    val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    val navigationUI = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+
+    // Test IMPL
+    val junit = "junit:junit:${Versions.junit}"
+    val extJunit = "androidx.test.ext:junit:${Versions.extJunit}"
+    val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,43 @@
+object Versions {
+
+    const val coreCoreKtx = "1.3.1"
+    const val kotlinVersion = "1.4.10"
+
+    const val appCompat = "1.2.0"
+    const val constraintLayout = "2.0.1"
+    const val cardView = "1.0.0"
+    const val recyclerView = "1.1.0"
+
+    const val timber = "4.7.1"
+
+    const val multidex = "2.0.0"
+
+    const val hawk = "2.0.1"
+
+    const val junit = "4.13"
+    const val extJunit = "1.1.2"
+    const val espressoCore = "3.3.0"
+
+    const val koin = "2.2.2"
+
+    const val glide = "4.11.0"
+
+    const val room = "2.2.5"
+
+    const val retrofit = "2.9.0"
+    const val gson = "2.8.6"
+    const val okhttp = "3.12.1"
+
+    const val coroutines = "1.3.0"
+
+    const val lifecycle = "2.2.0"
+    const val arch = "2.1.0"
+
+    const val rxJava = "2.2.5"
+    const val rxAndroid = "2.1.0"
+
+    const val androidMaterial = "1.3.0-alpha02"
+
+    const val navigation = "2.3.0"
+
+}
