d718a6794bb7f9cade037ac47de0572080c25550
Mehmet Agah <mehmeta.balbayy@gmail.com>
2021-04-04 01:34:25 +0300

    Splash space.gif added and ui improvement

--- app/src/main/java/mehmetbalbay/spaceApp/ui/splash/SplashActivity.kt
+++ app/src/main/java/mehmetbalbay/spaceApp/ui/splash/SplashActivity.kt
@@ -1,40 +1,35 @@
 package mehmetbalbay.spaceApp.ui.splash
 
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.Window
-import android.view.WindowManager
+import com.bumptech.glide.Glide
 import mehmetbalbay.spaceApp.R
 import mehmetbalbay.spaceApp.base.DatabindingActivity
 import mehmetbalbay.spaceApp.databinding.ActivitySplashBinding
+import mehmetbalbay.spaceApp.utils.Const
+import mehmetbalbay.spaceApp.utils.SharedPreferenceHelper
 import mehmetbalbay.spaceApp.utils.startCreateVehicleActivity
 import mehmetbalbay.spaceApp.utils.startMainActivity
-
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : DatabindingActivity() {
 
     private val binding: ActivitySplashBinding by binding(R.layout.activity_splash)
 
-    val SPLASH_DURATION: Long = 2000
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        // In Activity's onCreate() for instance
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val w: Window = window
-            w.setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-            )
-        }
+        Glide.with(this).load(R.raw.space).into(binding.spaceIconImage)
 
         Handler(Looper.getMainLooper()).postDelayed({
             this.finish()
-            startCreateVehicleActivity()
-        }, SPLASH_DURATION)
+            val isSaveVehicleData = SharedPreferenceHelper.getSharedData<Boolean>(Const.IS_VEHICLE_SAVE)
+            if (isSaveVehicleData == true) {
+                startMainActivity()
+            } else {
+                startCreateVehicleActivity()
+            }
+        }, Const.SPLASH_DURATION)
     }
 }
